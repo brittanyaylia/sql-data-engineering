57a6bc9cef52123cb9698ac667b1711df54c2bf0
brittanyaylia <whitakerbrittany18@gmail.com>
2021-04-10 11:18:08 -0700

    no. 8 completed

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -70,6 +70,9 @@ SELECT "Department_Emp".emp_no,
 		JOIN "Departments" on "Departments".dept_no = "Department_Emp".dept_no
 		WHERE dept_name = 'Sales' or dept_name = 'Development';
 		
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) FROM "Employees" GROUP BY last_name ORDER BY COUNT desc;
 		
 		
 		
