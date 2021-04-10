4041ac619e5060db8c5b28c4bf16b03ba68271c9
brittanyaylia <whitakerbrittany18@gmail.com>
2021-04-09 17:05:12 -0700

    no. 2 completed

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -1,14 +1,19 @@
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-Select e.emp_no, 
+SELECT e.emp_no, 
 	e.last_name, 
 	e.first_name, 
 	e.sex, 
 	s.salary
 	
-	from "Salaries" as s
-	Inner Join "Employees" as e 
-	on e.emp_no = s.emp_no;
+	FROM "Salaries" AS s
+	INNER JOIN "Employees" AS e 
+	ON e.emp_no = s.emp_no;
 	
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 
+SELECT "Employees".first_name,
+		"Employees".last_name,
+		"Employees".hire_date
+		FROM "Employees"
+		WHERE "Employees".hire_date >= '1986-01-01' AND "Employees".hire_date <= '1986-12-31'
